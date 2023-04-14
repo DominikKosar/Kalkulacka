929d032c554ec145989b14ae95ff70366d96dadd
Dominik Košař <dom707@seznam.cz>
2023-04-14 15:38:37 +0200

    Vytvoření formuláře pro kalkulačku

--- index.php
+++ index.php
@@ -7,6 +7,14 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <?php
+        include "classes.php";
+    ?>
+
+    <form action="index.php" method="post">
+        <input type="text" name="number1"><br>
+        <input type="text" name="number2"><br>
+        <input type="submit" value="odeslat" name="submit">
+    </form>
 </body>
 </html>
